6cc99b13c9e04662480db3e9692b68ec0832243b
Allan Jardine <allan@stargazer>
2018-01-29 16:54:50 +0000

    Editor 1.7.2 release

--- src/editor.ts
+++ src/editor.ts
@@ -208,7 +208,7 @@ interface ILeftJoin {
 export default class Editor extends NestedData {
 	public static Action = Action;
 
-	public static version: string = '1.7.1';
+	public static version: string = '1.7.2';
 
 	/**
 	 * Determine the request type from an HTTP request.
