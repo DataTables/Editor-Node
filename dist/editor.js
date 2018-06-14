6b2f5c322e728a996f20cc018fa0e97d1e8aba94
Allan Jardine <allan.jardine@sprymedia.co.uk>
2018-06-14 12:21:53 +0100

    Update version

--- dist/editor.js
+++ dist/editor.js
@@ -1583,7 +1583,7 @@ var Editor = /** @class */ (function (_super) {
         });
     };
     Editor.Action = Action;
-    Editor.version = '1.7.3';
+    Editor.version = '1.7.4';
     return Editor;
 }(nestedData_1.default));
 exports.default = Editor;
--- src/editor.ts
+++ src/editor.ts
@@ -208,7 +208,7 @@ interface ILeftJoin {
 export default class Editor extends NestedData {
 	public static Action = Action;
 
-	public static version: string = '1.7.3';
+	public static version: string = '1.7.4';
 
 	/**
 	 * Determine the request type from an HTTP request.
