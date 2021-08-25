278c03c6b5b7801e31fa1efae79e45762bab81ae
Allan Jardine <allan@datatables.net>
2021-08-25 13:36:03 +0000

    2.0.5 release

--- dist/editor.js
+++ dist/editor.js
@@ -2138,7 +2138,7 @@ var Editor = /** @class */ (function (_super) {
         });
     };
     Editor.Action = Action;
-    Editor.version = '2.0.4';
+    Editor.version = '2.0.5';
     return Editor;
 }(nestedData_1.default));
 exports.default = Editor;
--- src/editor.ts
+++ src/editor.ts
@@ -406,7 +406,7 @@ interface ILeftJoin {
 export default class Editor extends NestedData {
 	public static Action = Action;
 
-	public static version: string = '2.0.4';
+	public static version: string = '2.0.5';
 
 	/**
 	 * Determine the request type from an HTTP request.
