126d653e2b9e0f1315ff20e7223db34ed3878d0e
Allan Jardine <allan@datatables.net>
2025-11-14 16:43:48 +0000

    Release 2.5.1

--- dist/editor.js
+++ dist/editor.js
@@ -2328,7 +2328,7 @@ var Editor = /** @class */ (function (_super) {
         });
     };
     Editor.Action = Action;
-    Editor.version = '2.5.0';
+    Editor.version = '2.5.1';
     return Editor;
 }(nestedData_1.default));
 exports.default = Editor;
--- src/editor.ts
+++ src/editor.ts
@@ -458,7 +458,7 @@ type IGet = (id: string | string[], http) => Promise<IDtResponse>;
 export default class Editor extends NestedData {
 	public static Action = Action;
 
-	public static version: string = '2.5.0';
+	public static version: string = '2.5.1';
 
 	/**
 	 * Determine the request type from an HTTP request.
